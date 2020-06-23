f4a98cc106ad50119f6c13f115748ed737d1de40
Maik Riechel <m.riechel@codeelements.de>
2020-06-23 08:31:24 +0200

    Report issues related to news link url

---
+++ modules/core/src/main/java/de/deutschebahn/bahnhoflive/analytics/UncriticalIssueException.kt
@@ -0,0 +1,13 @@
+package de.deutschebahn.bahnhoflive.analytics
+
+class UncriticalIssueException : Exception {
+    constructor(message: String?) : super(message)
+    constructor(message: String?, cause: Throwable?) : super(message, cause)
+    constructor(cause: Throwable?) : super(cause)
+    constructor(
+        message: String?,
+        cause: Throwable?,
+        enableSuppression: Boolean,
+        writableStackTrace: Boolean
+    ) : super(message, cause, enableSuppression, writableStackTrace)
+}
