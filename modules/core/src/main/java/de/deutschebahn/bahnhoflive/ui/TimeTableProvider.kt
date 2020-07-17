9598414cb1bf010403dd1268cd6acb68f981ab12
Maik Riechel <m.riechel@codeelements.de>
2020-07-17 10:50:00 +0200

    BAHNHOFLIVE-1732 Use PTS instead of Hafas for nearby local transport station search

--- modules/core/src/main/java/de/deutschebahn/bahnhoflive/ui/TimeTableProvider.kt
+++ modules/core/src/main/java/de/deutschebahn/bahnhoflive/ui/TimeTableProvider.kt
@@ -5,16 +5,14 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import com.android.volley.VolleyError
 import de.deutschebahn.bahnhoflive.BaseApplication
-import de.deutschebahn.bahnhoflive.backend.BaseRestListener
 import de.deutschebahn.bahnhoflive.backend.VolleyRestListener
 import de.deutschebahn.bahnhoflive.backend.db.publictrainstation.model.StopPlace
-import de.deutschebahn.bahnhoflive.backend.hafas.LocalTransportFilter
 import de.deutschebahn.bahnhoflive.backend.hafas.model.HafasStation
-import de.deutschebahn.bahnhoflive.backend.hafas.model.ProductCategory
 import de.deutschebahn.bahnhoflive.repository.DbTimetableResource
 import de.deutschebahn.bahnhoflive.repository.LifecycleResourceClient
 import de.deutschebahn.bahnhoflive.repository.Station
 import de.deutschebahn.bahnhoflive.repository.StationResourceProvider
+import kotlin.math.roundToInt
 
 class TimeTableProvider {
 
@@ -33,61 +31,80 @@ class TimeTableProvider {
                 private var resourceClient: LifecycleResourceClient<Station, VolleyError>? = null
 
                 override fun onSuccess(payload: List<StopPlace>) {
-                    payload.firstOrNull()?.asInternalStation?.let { internalStation ->
-                        val stationResource = stationResourceProvider.getStationResource(internalStation.id)
+                    payload.firstOrNull { it.isDbStation }?.asInternalStation?.let { internalStation ->
+                        val stationResource =
+                            stationResourceProvider.getStationResource(internalStation.id)
 
                         stationResource.refresh()
 
-                        if (resourceClient != null) {
-                            resourceClient!!.releaseResource()
+                        resourceClient?.releaseResource()
+
+                        resourceClient = LifecycleResourceClient<Station, VolleyError>(owner,
+                            Observer { station ->
+                                val evaIds = station!!.evaIds
+                                requestHafasStations(
+                                    location,
+                                    evaIds.ids,
+                                    origin,
+                                    resultListener,
+                                    payload
+                                )
+
+                                resultListener.onDbTimeTableResourceAvailable(
+                                    DbTimetableResource(
+                                        internalStation
+                                    )
+                                )
+                            }, null,
+                            Observer { volleyError ->
+                                if (volleyError != null) {
+                                    requestHafasStations(
+                                        location,
+                                        null,
+                                        origin,
+                                        resultListener,
+                                        payload
+                                    )
+
+                                    resultListener.onDbTimeTableResourceAvailable(
+                                        DbTimetableResource(internalStation)
+                                    )
+                                }
+                            }).apply {
+                            observe(stationResource)
                         }
 
-                        resourceClient = LifecycleResourceClient(owner,
-                                Observer { station ->
-                                    val evaIds = station!!.evaIds
-                                    requestHafasStations(location, evaIds.ids, origin, resultListener)
+                    } ?: requestHafasStations(location, null, origin, resultListener, payload)
 
-                                    resultListener.onDbTimeTableResourceAvailable(DbTimetableResource(internalStation))
-                                }, null,
-                                Observer { volleyError ->
-                                    if (volleyError != null) {
-                                        requestHafasStations(location, null, origin, resultListener)
-                                        resultListener.onDbTimeTableResourceAvailable(
-                                            DbTimetableResource(internalStation)
-                                        )
-                                    }
-                                })
-                        resourceClient!!.observe(stationResource)
-                    } ?: requestHafasStations(location, null, origin, resultListener)
                 }
 
                 override fun onFail(reason: VolleyError) {
-                    requestHafasStations(location, null, origin, resultListener)
+                    resultListener.onFail(reason)
                 }
-            }, location = location, mixedResults = false)
+            }, location = location, mixedResults = true, limit = 100)
 
     private fun requestHafasStations(
         location: Location,
         evaIds: List<String>?,
         origin: String,
-        resultListener: StationLookupResultListener
+        resultListener: StationLookupResultListener,
+        stopPlaces: List<StopPlace>
     ) {
-        BaseApplication.get().repositories
-            .localTransportRepository.queryNearbyStations(
-            location.latitude,
-            location.longitude,
-            LocalTransportFilter(20, evaIds, ProductCategory.BITMASK_LOCAL_TRANSPORT),
-            object : BaseRestListener<List<HafasStation>>() {
-                override fun onSuccess(payload: List<HafasStation>) {
-                    resultListener.onHafasStationsAvailable(payload)
-                }
+        resultListener.onHafasStationsAvailable(
+            stopPlaces.mapNotNull {
+                it.takeIf { it.isLocalTransportStation }?.let { stopPlace ->
+                    HafasStation().also { hafasStation ->
+                        hafasStation.name = stopPlace.name
+                        hafasStation.extId = stopPlace.evaId
+                        stopPlace.location?.let {
+                            hafasStation.longitude = it.longitude
+                            hafasStation.latitude = it.latitude
+                        }
+                        hafasStation.dist = (stopPlace.distanceInKm * 1000).roundToInt()
+                    }
 
-                override fun onFail(reason: VolleyError) {
-                    super.onFail(reason)
-                    resultListener.onFail(reason)
                 }
-
-            }, origin, 2000
+            }
         )
     }
 
