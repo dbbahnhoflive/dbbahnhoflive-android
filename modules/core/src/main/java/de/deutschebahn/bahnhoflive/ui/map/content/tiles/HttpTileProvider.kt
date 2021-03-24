ed2c44ea646872fcce63b9e1ed1cb59537ed690d
Maik Riechel <m.riechel@codeelements.de>
2021-03-24 15:21:26 +0100

    BAHNHOFLIVE-1945 Use DB map provider at selected stations

--- modules/core/src/main/java/de/deutschebahn/bahnhoflive/ui/map/content/tiles/HttpTileProvider.kt
+++ modules/core/src/main/java/de/deutschebahn/bahnhoflive/ui/map/content/tiles/HttpTileProvider.kt
@@ -3,15 +3,16 @@ package de.deutschebahn.bahnhoflive.ui.map.content.tiles
 import com.google.android.gms.maps.model.Tile
 import com.google.android.gms.maps.model.TileProvider
 import java.net.URL
+import java.net.URLConnection
 
 abstract class HttpTileProvider(
     val width: Int,
     val height: Int
 ) : TileProvider {
 
-    open fun getTileUrl(x: Int, y: Int, zoom: Int): URL? = null
+    protected open fun getTileUrl(x: Int, y: Int, zoom: Int): URL? = null
 
-    open fun openConnection(url: URL) = url.openConnection()
+    protected open fun openConnection(url: URL): URLConnection = url.openConnection()
 
     override fun getTile(x: Int, y: Int, zoom: Int): Tile =
         try {
