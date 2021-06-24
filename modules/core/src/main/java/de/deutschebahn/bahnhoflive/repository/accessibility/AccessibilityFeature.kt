9da5df21d4643de0dec5573ec9ab5933f088634e
Maik Riechel <m.riechel@codeelements.de>
2021-06-24 09:36:11 +0200

    BAHNHOFLIVE-2009 Android - Integration Legende zu Barrierefreiheitsmerkmalen

--- modules/core/src/main/java/de/deutschebahn/bahnhoflive/repository/accessibility/AccessibilityFeature.kt
+++ modules/core/src/main/java/de/deutschebahn/bahnhoflive/repository/accessibility/AccessibilityFeature.kt
@@ -6,29 +6,61 @@ import de.deutschebahn.bahnhoflive.R
 enum class AccessibilityFeature(
     val tag: String,
     @StringRes val label: Int,
+    @StringRes val description: Int,
     @StringRes val contentDescription: Int? = null
 ) {
 
-    STEP_FREE_ACCESS("stepFreeAccess", R.string.accessibilityStepFreeAccess),
+    STEP_FREE_ACCESS(
+        "stepFreeAccess",
+        R.string.accessibilityStepFreeAccess,
+        R.string.accessibilityDescriptionStepFreeAccess
+    ),
     STANDARD_PLATFORM_HEIGHT(
         "standardPlatformHeight",
         R.string.accessibilityStandardPlatformHeight,
+        R.string.accessibilityDescriptionStandardPlatformHeight,
         R.string.sr_accessibilityStandardPlatformHeight
     ),
     PASSENGER_INFORMATION_DISPLAY(
         "passengerInformationDisplay",
-        R.string.accessibilityPassengerInformationDisplay
+        R.string.accessibilityPassengerInformationDisplay,
+        R.string.accessibilityDescriptionPassengerInformationDisplay
     ),
     AUDIBLE_SIGNALS_AVAILABLE(
         "audibleSignalsAvailable",
-        R.string.accessibilityAudibleSignalsAvailable
+        R.string.accessibilityAudibleSignalsAvailable,
+        R.string.accessibilityDescriptionAudibleSignalsAvailable
     ),
-    TACTILE_PLATFORM_ACCESS("tactilePlatformAccess", R.string.accessibilityTactilePlatformAccess),
-    TACTILE_GUIDING_STRIPS("tactileGuidingStrips", R.string.accessibilityTactileGuidingStrips),
-    TACTILE_HANDRAIL_LABEL("tactileHandrailLabel", R.string.accessibilityTactileHandrailLabel),
-    STAIRS_MARKING("stairsMarking", R.string.accessibilityStairsMarking),
-    PLATFORM_SIGN("platformSign", R.string.accessibilityPlatformSign),
-    AUTOMATIC_DOOR("automaticDoor", R.string.accessibilityAutomaticDoor);
+    TACTILE_PLATFORM_ACCESS(
+        "tactilePlatformAccess",
+        R.string.accessibilityTactilePlatformAccess,
+        R.string.accessibilityDescriptionTactilePlatformAccess
+    ),
+    TACTILE_GUIDING_STRIPS(
+        "tactileGuidingStrips",
+        R.string.accessibilityTactileGuidingStrips,
+        R.string.accessibilityDescriptionTactileGuidingStrips
+    ),
+    TACTILE_HANDRAIL_LABEL(
+        "tactileHandrailLabel",
+        R.string.accessibilityTactileHandrailLabel,
+        R.string.accessibilityDescriptionTactileHandrailLabel
+    ),
+    STAIRS_MARKING(
+        "stairsMarking",
+        R.string.accessibilityStairsMarking,
+        R.string.accessibilityDescriptionStairsMarking
+    ),
+    PLATFORM_SIGN(
+        "platformSign",
+        R.string.accessibilityPlatformSign,
+        R.string.accessibilityDescriptionPlatformSign
+    ),
+    AUTOMATIC_DOOR(
+        "automaticDoor",
+        R.string.accessibilityAutomaticDoor,
+        R.string.accessibilityDescriptionAutomaticDoor
+    );
 
     companion object {
         val VALUES = values()
