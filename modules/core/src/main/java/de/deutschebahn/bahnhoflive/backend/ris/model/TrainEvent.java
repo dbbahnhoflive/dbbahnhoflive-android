97cc34bd010adbcaba6fcb60649a7af934589c6d
Maik Riechel <m.riechel@codeelements.de>
2021-10-26 15:16:58 +0200

    BAHNHOFLIVE-2066 Include arrivals in content search

--- modules/core/src/main/java/de/deutschebahn/bahnhoflive/backend/ris/model/TrainEvent.java
+++ modules/core/src/main/java/de/deutschebahn/bahnhoflive/backend/ris/model/TrainEvent.java
@@ -8,26 +8,31 @@ package de.deutschebahn.bahnhoflive.backend.ris.model;
 
 import androidx.annotation.NonNull;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Comparator;
 
 import de.deutschebahn.bahnhoflive.R;
 
 public enum TrainEvent {
 
-    DEPARTURE(TrainInfo::getDeparture, "departure", true, R.string.sr_timetable_type_departure),
+    DEPARTURE(TrainInfo::getDeparture, "departure", true, R.string.sr_timetable_type_departure, "Ab"),
 
-    ARRIVAL(TrainInfo::getArrival, "arrival", false, R.string.sr_timetable_type_arrival);
+    ARRIVAL(TrainInfo::getArrival, "arrival", false, R.string.sr_timetable_type_arrival, "An");
 
 
     public final boolean isDeparture;
     public final int contentDescriptionPhrase;
+    @NotNull
+    public final String timeLabel;
 
-    TrainEvent(MovementRetriever movementRetriever, String trackKey, boolean isDeparture, int contentDescriptionPhrase) {
+    TrainEvent(MovementRetriever movementRetriever, String trackKey, boolean isDeparture, int contentDescriptionPhrase, @NonNull String timeLabel) {
         this.movementRetriever = movementRetriever;
         this.contentDescriptionPhrase = contentDescriptionPhrase;
         this.comparator = new TrainInfo.Comparator(this);
         this.trackKey = trackKey;
         this.isDeparture = isDeparture;
+        this.timeLabel = timeLabel;
     }
 
     public interface MovementRetriever {
