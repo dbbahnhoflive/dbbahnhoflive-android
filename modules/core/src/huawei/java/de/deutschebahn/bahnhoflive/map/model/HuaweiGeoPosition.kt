0fa977e459b1f89b5ee91712765cb275861acd22
Maik Riechel <m.riechel@codeelements.de>
2021-11-25 09:26:27 +0100

    BAHNHOFLIVE-2068 Fix mixed up coordinates

--- modules/core/src/huawei/java/de/deutschebahn/bahnhoflive/map/model/HuaweiGeoPosition.kt
+++ modules/core/src/huawei/java/de/deutschebahn/bahnhoflive/map/model/HuaweiGeoPosition.kt
@@ -2,6 +2,6 @@ package de.deutschebahn.bahnhoflive.map.model
 
 import com.huawei.hms.maps.model.LatLng
 
-fun LatLng.toGeoPosition() = GeoPosition(longitude, latitude)
+fun LatLng.toGeoPosition() = GeoPosition(latitude, longitude)
 
 fun GeoPosition.toLatLng() = LatLng(latitude, longitude)
--- modules/core/src/main/java/de/deutschebahn/bahnhoflive/ui/map/content/tiles/HttpAppTileProvider.kt
+++ modules/core/src/main/java/de/deutschebahn/bahnhoflive/ui/map/content/tiles/HttpAppTileProvider.kt
@@ -1,5 +1,6 @@
 package de.deutschebahn.bahnhoflive.ui.map.content.tiles
 
+import android.util.Log
 import de.deutschebahn.bahnhoflive.map.model.AppTile
 import de.deutschebahn.bahnhoflive.map.model.AppTileProvider
 import java.net.URL
@@ -15,17 +16,21 @@ abstract class HttpAppTileProvider(
     protected open fun openConnection(url: URL): URLConnection = url.openConnection()
 
     override fun getTile(x: Int, y: Int, zoom: Int): AppTile? =
-        try {
             getTileUrl(x, y, zoom)?.let { url ->
-                AppTile(
-                    width,
-                    height,
-                    openConnection(url).getInputStream().buffered().use { stream ->
-                        stream.readBytes()
-                    })
+                try {
+                    AppTile(
+                        width,
+                        height,
+                        openConnection(url).getInputStream().buffered().use { stream ->
+                            stream.readBytes()
+                        }).also {
+                        Log.d(HttpAppTileProvider::class.java.simpleName, "Created tile $url")
+                    }
+                } catch (e: Exception) {
+                    Log.i(
+                        HttpAppTileProvider::class.java.simpleName, "Error loading tile $url", e
+                    )
+                    null
+                }
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            null
-        }
 }
