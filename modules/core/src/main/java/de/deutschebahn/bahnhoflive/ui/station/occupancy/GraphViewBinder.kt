aa17b46ae829f2d2195044251d1060838c70720b
Maik Riechel <m.riechel@codeelements.de>
2022-08-04 15:23:44 +0200

    ViewBinding migration in progress

--- modules/core/src/main/java/de/deutschebahn/bahnhoflive/ui/station/occupancy/GraphViewBinder.kt
+++ modules/core/src/main/java/de/deutschebahn/bahnhoflive/ui/station/occupancy/GraphViewBinder.kt
@@ -7,20 +7,18 @@
 package de.deutschebahn.bahnhoflive.ui.station.occupancy
 
 import android.view.View
+import de.deutschebahn.bahnhoflive.databinding.IncludeGraphBinding
 import de.deutschebahn.bahnhoflive.repository.occupancy.model.DailyOccupancy
 import de.deutschebahn.bahnhoflive.repository.occupancy.model.HourlyOccupancy
-import kotlinx.android.synthetic.main.include_graph.view.*
-import kotlinx.android.synthetic.main.include_graph_bar_highlight.view.*
-import kotlinx.android.synthetic.main.include_graph_slot.view.*
-import kotlinx.android.synthetic.main.include_occupancy_graph_inter_slot.view.*
-import kotlinx.android.synthetic.main.include_occupancy_time_label.view.*
 import kotlin.math.ceil
 import kotlin.math.floor
 
 class GraphViewBinder(
-    parent: View,
+    parentView: View,
     private val timeTextWidth: Float
 ) {
+
+    private val parent = IncludeGraphBinding.bind(parentView)
 
     private val graphView = parent.graph.also {
         it.addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
@@ -31,58 +29,58 @@ class GraphViewBinder(
     }
 
     private val slotViews = listOf(
-        graphView.occupancyGraphSlot00,
-        graphView.occupancyGraphSlot01,
-        graphView.occupancyGraphSlot02,
-        graphView.occupancyGraphSlot03,
-        graphView.occupancyGraphSlot04,
-        graphView.occupancyGraphSlot05,
-        graphView.occupancyGraphSlot06,
-        graphView.occupancyGraphSlot07,
-        graphView.occupancyGraphSlot08,
-        graphView.occupancyGraphSlot09,
-        graphView.occupancyGraphSlot10,
-        graphView.occupancyGraphSlot11,
-        graphView.occupancyGraphSlot12,
-        graphView.occupancyGraphSlot13,
-        graphView.occupancyGraphSlot14,
-        graphView.occupancyGraphSlot15,
-        graphView.occupancyGraphSlot16,
-        graphView.occupancyGraphSlot17,
-        graphView.occupancyGraphSlot18,
-        graphView.occupancyGraphSlot19,
-        graphView.occupancyGraphSlot20,
-        graphView.occupancyGraphSlot21,
-        graphView.occupancyGraphSlot22,
-        graphView.occupancyGraphSlot23,
-        graphView.occupancyGraphSlot24
+        parent.occupancyGraphSlot00,
+        parent.occupancyGraphSlot01,
+        parent.occupancyGraphSlot02,
+        parent.occupancyGraphSlot03,
+        parent.occupancyGraphSlot04,
+        parent.occupancyGraphSlot05,
+        parent.occupancyGraphSlot06,
+        parent.occupancyGraphSlot07,
+        parent.occupancyGraphSlot08,
+        parent.occupancyGraphSlot09,
+        parent.occupancyGraphSlot10,
+        parent.occupancyGraphSlot11,
+        parent.occupancyGraphSlot12,
+        parent.occupancyGraphSlot13,
+        parent.occupancyGraphSlot14,
+        parent.occupancyGraphSlot15,
+        parent.occupancyGraphSlot16,
+        parent.occupancyGraphSlot17,
+        parent.occupancyGraphSlot18,
+        parent.occupancyGraphSlot19,
+        parent.occupancyGraphSlot20,
+        parent.occupancyGraphSlot21,
+        parent.occupancyGraphSlot22,
+        parent.occupancyGraphSlot23,
+        parent.occupancyGraphSlot24
     )
 
     private val interSlotViews = listOf(
-        graphView.occupancyInterSlot00,
-        graphView.occupancyInterSlot01,
-        graphView.occupancyInterSlot02,
-        graphView.occupancyInterSlot03,
-        graphView.occupancyInterSlot04,
-        graphView.occupancyInterSlot05,
-        graphView.occupancyInterSlot06,
-        graphView.occupancyInterSlot07,
-        graphView.occupancyInterSlot08,
-        graphView.occupancyInterSlot09,
-        graphView.occupancyInterSlot10,
-        graphView.occupancyInterSlot11,
-        graphView.occupancyInterSlot12,
-        graphView.occupancyInterSlot13,
-        graphView.occupancyInterSlot14,
-        graphView.occupancyInterSlot15,
-        graphView.occupancyInterSlot16,
-        graphView.occupancyInterSlot17,
-        graphView.occupancyInterSlot18,
-        graphView.occupancyInterSlot19,
-        graphView.occupancyInterSlot20,
-        graphView.occupancyInterSlot21,
-        graphView.occupancyInterSlot22,
-        graphView.occupancyInterSlot23
+        parent.occupancyInterSlot00,
+        parent.occupancyInterSlot01,
+        parent.occupancyInterSlot02,
+        parent.occupancyInterSlot03,
+        parent.occupancyInterSlot04,
+        parent.occupancyInterSlot05,
+        parent.occupancyInterSlot06,
+        parent.occupancyInterSlot07,
+        parent.occupancyInterSlot08,
+        parent.occupancyInterSlot09,
+        parent.occupancyInterSlot10,
+        parent.occupancyInterSlot11,
+        parent.occupancyInterSlot12,
+        parent.occupancyInterSlot13,
+        parent.occupancyInterSlot14,
+        parent.occupancyInterSlot15,
+        parent.occupancyInterSlot16,
+        parent.occupancyInterSlot17,
+        parent.occupancyInterSlot18,
+        parent.occupancyInterSlot19,
+        parent.occupancyInterSlot20,
+        parent.occupancyInterSlot21,
+        parent.occupancyInterSlot22,
+        parent.occupancyInterSlot23
     )
 
     private var max: Int = MAX_FALLBACK
@@ -114,33 +112,33 @@ class GraphViewBinder(
             ceil(dailyOccupancy.hourlyOccupancies.size / timeScaleCount.toDouble()).toInt()
 
 
-        slotViews.forEachIndexed { hourIndex, view ->
+        slotViews.forEachIndexed { hourIndex, graphSlotBinding ->
             val hourlyOccupancy = dailyOccupancy.hourlyOccupancies.getOrNull(hourIndex)
             if (hourlyOccupancy == null) {
-                view.visibility = View.GONE
+                graphSlotBinding.root.visibility = View.GONE
             } else {
-                view.visibility = View.VISIBLE
-                offset(view.graphBar, hourlyOccupancy.average)
+                graphSlotBinding.root.visibility = View.VISIBLE
+                offset(graphSlotBinding.graphBar.root, hourlyOccupancy.average)
 
-                view.graphBarHighlighted.run {
+                graphSlotBinding.graphBarHighlighted.run {
                     if (hourlyOccupancy == currentOccupancy) {
-                        offset(this, hourlyOccupancy.current)
-                        visibility = View.VISIBLE
+                        offset(root, hourlyOccupancy.current)
+                        root.visibility = View.VISIBLE
                         hourlyOccupancy.average?.let { average ->
                             hourlyOccupancy.current?.let { current ->
                                 average <= current
                             }
                         } ?: false
                     } else {
-                        view.graphBarOverlay.visibility = View.GONE
-                        visibility = View.GONE
+                        graphSlotBinding.graphBarOverlay.root.visibility = View.GONE
+                        root.visibility = View.GONE
                         false
                     }
                 }.also { showOverlayedAverage ->
-                    with(view.graphBarOverlay) {
-                        visibility = if (showOverlayedAverage) {
-                            offset(this, hourlyOccupancy.average)
-                            background?.alpha = 51 // 20%
+                    with(graphSlotBinding.graphBarOverlay) {
+                        root.visibility = if (showOverlayedAverage) {
+                            offset(root, hourlyOccupancy.average)
+                            root.background?.alpha = 51 // 20%
                             View.VISIBLE
                         } else {
                             View.GONE
@@ -150,20 +148,23 @@ class GraphViewBinder(
             }
         }
 
-        interSlotViews.forEachIndexed { index, view ->
+        interSlotViews.forEachIndexed { index, occupancyGraphInterSlotBinding ->
             val hourlyOccupancy = dailyOccupancy.hourlyOccupancies.getOrNull(index + 1)
             if (hourlyOccupancy == null) {
-                view.visibility = View.GONE
+                occupancyGraphInterSlotBinding.root.visibility = View.GONE
             } else {
-                view.visibility = View.VISIBLE
+                occupancyGraphInterSlotBinding.root.visibility = View.VISIBLE
                 if ((timeScalePeriod / 1.5f + index + 1).toInt() % timeScalePeriod == 0) {
-                    view.timeScale.text = "${hourlyOccupancy.hourOfDay}:00"
+                    occupancyGraphInterSlotBinding.occupancyTimeLabel.timeScale.text =
+                        "${hourlyOccupancy.hourOfDay}:00"
 
-                    view.scaleLine.visibility = View.VISIBLE
-                    view.timeScale.visibility = View.VISIBLE
+                    occupancyGraphInterSlotBinding.scaleLine.visibility = View.VISIBLE
+                    occupancyGraphInterSlotBinding.occupancyTimeLabel.timeScale.visibility =
+                        View.VISIBLE
                 } else {
-                    view.timeScale.visibility = View.GONE
-                    view.scaleLine.visibility = View.GONE
+                    occupancyGraphInterSlotBinding.occupancyTimeLabel.timeScale.visibility =
+                        View.GONE
+                    occupancyGraphInterSlotBinding.scaleLine.visibility = View.GONE
                 }
             }
         }
