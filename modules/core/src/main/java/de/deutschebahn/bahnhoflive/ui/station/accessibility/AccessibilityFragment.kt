563f97a13128ae43db26a80a01e7b0c43a17f1d2
Maik Riechel <m.riechel@codeelements.de>
2022-08-08 13:01:44 +0200

    ViewBinding migration in progress

--- modules/core/src/main/java/de/deutschebahn/bahnhoflive/ui/station/accessibility/AccessibilityFragment.kt
+++ modules/core/src/main/java/de/deutschebahn/bahnhoflive/ui/station/accessibility/AccessibilityFragment.kt
@@ -14,45 +14,46 @@ import androidx.recyclerview.widget.ConcatAdapter
 import de.deutschebahn.bahnhoflive.R
 import de.deutschebahn.bahnhoflive.analytics.TrackingManager
 import de.deutschebahn.bahnhoflive.backend.db.ris.model.AccessibilityStatus
+import de.deutschebahn.bahnhoflive.databinding.FragmentAccessibilityBinding
+import de.deutschebahn.bahnhoflive.databinding.IncludeAccessibilityElevatorLinkBinding
+import de.deutschebahn.bahnhoflive.databinding.IncludeAccessibilityHeaderBinding
 import de.deutschebahn.bahnhoflive.repository.LoadingStatus
 import de.deutschebahn.bahnhoflive.repository.accessibility.AccessibilityFeature
 import de.deutschebahn.bahnhoflive.ui.station.StationViewModel
 import de.deutschebahn.bahnhoflive.util.PhoneIntent
 import de.deutschebahn.bahnhoflive.view.*
-import kotlinx.android.synthetic.main.fragment_accessibility.*
-import kotlinx.android.synthetic.main.fragment_accessibility.view.*
-import kotlinx.android.synthetic.main.include_accessibility_elevator_link.view.*
-import kotlinx.android.synthetic.main.include_accessibility_header.view.*
-import kotlinx.android.synthetic.main.titlebar_static.view.*
 
 class AccessibilityFragment : Fragment(R.layout.fragment_accessibility) {
 
     val viewModel by activityViewModels<StationViewModel>()
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.screen_title.setText(R.string.accessibility_title)
+        val fragmentAccessibilityBinding = FragmentAccessibilityBinding.bind(view)
+
+        fragmentAccessibilityBinding.commonTitlebar.staticTitleBar.screenTitle.setText(R.string.accessibility_title)
 
         val progressAdapter = SimpleAdapter(
-            view.recycler.inflate(R.layout.item_progress)
+            fragmentAccessibilityBinding.recycler.inflate(R.layout.item_progress)
         )
 
-        val headerView = view.recycler.inflate(
-            R.layout.include_accessibility_header
+        val includeAccessibilityHeaderBinding = IncludeAccessibilityHeaderBinding.inflate(
+            layoutInflater, fragmentAccessibilityBinding.recycler, false
         ).apply {
             key.setOnClickListener {
                 AccessibilityKeyFragment().show(childFragmentManager, null)
             }
 
-            filter.contentDescription = getText(R.string.accessibilityFilterButton)
+            filter.filter.contentDescription = getText(R.string.accessibilityFilterButton)
 
             filterAction.setOnClickListener {
                 PlatformSelectionFragment().show(childFragmentManager, "platformSelection")
             }
         }
         val headerAdapter = SimpleAdapter(
-            headerView
+            includeAccessibilityHeaderBinding.root
         )
 
         val accessibilityAdapter = BaseListAdapter(
@@ -75,11 +76,15 @@ class AccessibilityFragment : Fragment(R.layout.fragment_accessibility) {
         )
 
         val elevatorsLinkOptionalAdapter = OptionalAdapter(
-            view.recycler.inflate(R.layout.include_accessibility_elevator_link).apply {
-                elevators_link.setOnClickListener {
+            IncludeAccessibilityElevatorLinkBinding.inflate(
+                layoutInflater,
+                fragmentAccessibilityBinding.recycler,
+                false
+            ).apply {
+                elevatorsLink.setOnClickListener {
                     viewModel.stationNavigation?.showElevators()
                 }
-            },
+            }.root,
             false
         )
 
@@ -89,10 +94,10 @@ class AccessibilityFragment : Fragment(R.layout.fragment_accessibility) {
             elevatorsLinkOptionalAdapter
         )
 
-        view.recycler.adapter = progressAdapter
+        fragmentAccessibilityBinding.recycler.adapter = progressAdapter
 
-        headerView.phone?.setOnClickListener {
-            startActivity(PhoneIntent(view.phone.text.toString()))
+        includeAccessibilityHeaderBinding.phone.setOnClickListener {
+            startActivity(PhoneIntent(includeAccessibilityHeaderBinding.phone.text.toString()))
         }
 
         val accessibilityFeaturesResource = viewModel.accessibilityFeaturesResource.apply {
@@ -102,11 +107,11 @@ class AccessibilityFragment : Fragment(R.layout.fragment_accessibility) {
         viewModel.accessibilityPlatformsAndSelectedLiveData.observe(viewLifecycleOwner) { platformsAndSelection ->
             with(platformsAndSelection?.first) {
                 if (isNullOrEmpty()) {
-                    headerView.steplessAccessHint.visibility = View.GONE
+                    includeAccessibilityHeaderBinding.steplessAccessHint.visibility = View.GONE
                 } else {
-                    headerView.steplessAccessHint.visibility = View.VISIBLE
+                    includeAccessibilityHeaderBinding.steplessAccessHint.visibility = View.VISIBLE
 
-                    headerView.steplessAccessHint.setText(when {
+                    includeAccessibilityHeaderBinding.steplessAccessHint.setText(when {
                         all { platform ->
                             platform.accessibility[AccessibilityFeature.STEP_FREE_ACCESS] == AccessibilityStatus.AVAILABLE
                         } -> R.string.accessibilityStepFreeAll
@@ -119,17 +124,19 @@ class AccessibilityFragment : Fragment(R.layout.fragment_accessibility) {
             }
 
             platformsAndSelection?.second?.also { platform ->
-                headerView.selectedPlatform.text = "Gleis ${platform.name}"
-                headerView.filter.isSelected = true
-                headerView.selectPlatformInvitation.visibility = View.GONE
+                includeAccessibilityHeaderBinding.selectedPlatform.text = "Gleis ${platform.name}"
+                includeAccessibilityHeaderBinding.filter.filter.isSelected = true
+                includeAccessibilityHeaderBinding.selectPlatformInvitation.visibility = View.GONE
                 accessibilityAdapter.submitList(platform.accessibility.filter { accessibility ->
                     accessibility.component2() == AccessibilityStatus.AVAILABLE
                 }.toList())
             } ?: kotlin.run {
                 if (!platformsAndSelection?.first.isNullOrEmpty()) {
-                    headerView.selectedPlatform.text = "Kein Gleis ausgewählt"
-                    headerView.filter.isSelected = false
-                    headerView.selectPlatformInvitation.visibility = View.VISIBLE
+                    includeAccessibilityHeaderBinding.selectedPlatform.text =
+                        "Kein Gleis ausgewählt"
+                    includeAccessibilityHeaderBinding.filter.filter.isSelected = false
+                    includeAccessibilityHeaderBinding.selectPlatformInvitation.visibility =
+                        View.VISIBLE
                 }
 
                 accessibilityAdapter.submitList(emptyList())
@@ -137,16 +144,16 @@ class AccessibilityFragment : Fragment(R.layout.fragment_accessibility) {
 
         }
 
-        view.refresher.setOnRefreshListener {
+        fragmentAccessibilityBinding.refresher.setOnRefreshListener {
             accessibilityFeaturesResource.refresh()
         }
 
         accessibilityFeaturesResource.loadingStatus.observe(viewLifecycleOwner) { loadingStatus ->
             if (loadingStatus == LoadingStatus.IDLE) {
-                view.refresher.isRefreshing = false
+                fragmentAccessibilityBinding.refresher.isRefreshing = false
 
-                if (recycler.adapter != concatAdapter) {
-                    recycler.adapter = concatAdapter
+                if (fragmentAccessibilityBinding.recycler.adapter != concatAdapter) {
+                    fragmentAccessibilityBinding.recycler.adapter = concatAdapter
                 }
             }
         }
@@ -169,13 +176,13 @@ class AccessibilityFragment : Fragment(R.layout.fragment_accessibility) {
             addSource(accessibilityFeaturesResource.data, onChanged)
         }.distinctUntilChanged().observe(viewLifecycleOwner) { noData ->
             if (noData) {
-                headerView.steplessAccessHint.visibility = View.GONE
-                headerView.filter.visibility = View.GONE
-                headerView.selectPlatformInvitation.visibility = View.GONE
-                headerView.selectedPlatform.text = "Keine Daten verfügbar"
+                includeAccessibilityHeaderBinding.steplessAccessHint.visibility = View.GONE
+                includeAccessibilityHeaderBinding.filter.filter.visibility = View.GONE
+                includeAccessibilityHeaderBinding.selectPlatformInvitation.visibility = View.GONE
+                includeAccessibilityHeaderBinding.selectedPlatform.text = "Keine Daten verfügbar"
             } else {
-                headerView.steplessAccessHint.visibility = View.VISIBLE
-                headerView.filter.visibility = View.VISIBLE
+                includeAccessibilityHeaderBinding.steplessAccessHint.visibility = View.VISIBLE
+                includeAccessibilityHeaderBinding.filter.filter.visibility = View.VISIBLE
             }
         }
 
