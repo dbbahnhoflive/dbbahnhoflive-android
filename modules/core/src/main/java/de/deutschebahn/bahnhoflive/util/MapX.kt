bf563f1e6ca4743b4965500a4949d51da812c846
Codeelements_Ruhe <c.ruhe@codeelements.de>
2022-11-03 14:36:20 +0100

    fix map-preselection

--- modules/core/src/main/java/de/deutschebahn/bahnhoflive/util/MapX.kt
+++ modules/core/src/main/java/de/deutschebahn/bahnhoflive/util/MapX.kt
@@ -1,10 +1,10 @@
 package de.deutschebahn.bahnhoflive.util
 
-import android.app.Activity
 import android.content.Intent
 import androidx.fragment.app.Fragment
 import de.deutschebahn.bahnhoflive.BaseApplication
 import de.deutschebahn.bahnhoflive.ui.map.MapConsentDialogFragment
+import de.deutschebahn.bahnhoflive.ui.map.MapPresetProvider
 import de.deutschebahn.bahnhoflive.ui.map.OnMapConsentDialogListener
 
 fun <K, V> Map<K, V>?.asMutable() =
@@ -16,7 +16,12 @@ fun startMapActivityIfConsent(fragment: Fragment, createMapIntentFunction: () ->
         val mp = MapConsentDialogFragment()
         mp.setOnMapConsentDialogListener(object : OnMapConsentDialogListener {
             override fun onConsentAccepted() {
-                createMapIntentFunction()?.let{fragment.startActivity(it)}
+                createMapIntentFunction()?.let{
+                    if (fragment is MapPresetProvider) {
+                        (fragment as MapPresetProvider).prepareMapIntent(it)
+                    }
+                    fragment.startActivity(it)
+                }
             }
         })
         mp.show(fragment.parentFragmentManager, null)
