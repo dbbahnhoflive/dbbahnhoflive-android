6d636b4972718560e4756381c46983adba8b2ab3
Codeelements_Ruhe <c.ruhe@codeelements.de>
2022-11-03 15:10:09 +0100

    create

--- modules/core/src/main/java/de/deutschebahn/bahnhoflive/util/MapX.kt
+++ modules/core/src/main/java/de/deutschebahn/bahnhoflive/util/MapX.kt
@@ -1,11 +1,23 @@
 package de.deutschebahn.bahnhoflive.util
 
+import android.Manifest
 import android.app.Activity
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import de.deutschebahn.bahnhoflive.BaseApplication
+import androidx.lifecycle.ViewModelProvider
+import de.deutschebahn.bahnhoflive.BaseActivity
+import de.deutschebahn.bahnhoflive.R
 import de.deutschebahn.bahnhoflive.ui.map.MapConsentDialogFragment
+import de.deutschebahn.bahnhoflive.ui.map.MapPresetProvider
+import de.deutschebahn.bahnhoflive.ui.map.MapViewModel
 import de.deutschebahn.bahnhoflive.ui.map.OnMapConsentDialogListener
+
 
 fun <K, V> Map<K, V>?.asMutable() =
     this?.toMutableMap() ?: mutableMapOf()
@@ -109,7 +121,7 @@ class GoogleLocationPermissions {
                 baseActivity.requestAtleastOneOfThePermissions(permissions, permissionResponse)
             }
             else
-             permissionResponse(true)
+                permissionResponse(true)
         }
 
 
@@ -120,13 +132,63 @@ class GoogleLocationPermissions {
             fun permissionResponse(allowed: Boolean) {
                 // google-permissions ok, ask for db-consent
                 if (allowed) {
-                        createMapIntentFunction()?.let { fragment.startActivity(it) }
+                    createMapIntentFunction()?.let {
+                        if (fragment is MapPresetProvider) {
+                            (fragment as MapPresetProvider).prepareMapIntent(it)
+                        }
+                        fragment.startActivity(it)
+                    }
                 }
             }
-        })
-        mp.show(fragment.parentFragmentManager, null)
-    } else {
-        createMapIntentFunction()?.let{fragment.startActivity(it)}
-    }
 
+            fun showMapButAskForLocationPermissionsIfNecessary(baseActivity: BaseActivity) {
+                // consent if ok, but - if needed - ask for Location-Permissions
+                askForGoogleLocationPermissionIfNecessary(baseActivity, ::permissionResponse)
+            }
+
+            try {
+                val baseActivity: BaseActivity? = fragment.requireActivity() as? BaseActivity
+
+                if (baseActivity == null) {
+                    val activityName: String? = fragment.activity?.localClassName
+                    if (activityName != null)
+                        Log.d(
+                            "cr",
+                            "class $activityName has to inherit from BaseActivity to use map !!!!!!!!!"
+                        )
+                    else
+                        Log.d(
+                            "cr",
+                            "activity from class ${fragment.javaClass.name} has to inherit from BaseActivity to use map !!!!!!!!!"
+                        )
+
+                } else {
+
+                    var mapViewModel: MapViewModel =
+                        ViewModelProvider(baseActivity)[MapViewModel::class.java]
+
+                    if (mapViewModel.mapConsentedLiveData.value == false) {
+                        MapConsentDialogFragment().setOnMapConsentDialogListener(object :
+                            OnMapConsentDialogListener {
+
+                            override fun onConsentAccepted() {
+                                showMapButAskForLocationPermissionsIfNecessary(baseActivity)
+                            }
+
+                        })
+                            .show(fragment.parentFragmentManager, null)
+                    } else
+                        showMapButAskForLocationPermissionsIfNecessary(baseActivity)
+
+
+                }
+            } catch (e: Exception) {
+                e.message?.let { Log.d("cr", it) }
+            }
+        }
+
+
+
+
+    }
 }
