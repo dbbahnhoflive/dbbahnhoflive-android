e0590637f2ba8afefa2debe363a623751516b3a2
Codeelements_Ruhe <c.ruhe@codeelements.de>
2023-03-10 10:29:36 +0100

    DbTimetablesFragment converted from java -> kt

--- modules/core/src/main/java/de/deutschebahn/bahnhoflive/ui/station/timetable/DbTimetableFragment.kt
+++ modules/core/src/main/java/de/deutschebahn/bahnhoflive/ui/station/timetable/DbTimetableFragment.kt
@@ -3,241 +3,199 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+package de.deutschebahn.bahnhoflive.ui.station.timetable
 
-package de.deutschebahn.bahnhoflive.ui.station.timetable;
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ViewAnimator
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import de.deutschebahn.bahnhoflive.R
+import de.deutschebahn.bahnhoflive.analytics.TrackingManager
+import de.deutschebahn.bahnhoflive.analytics.TrackingManager.Companion.fromActivity
+import de.deutschebahn.bahnhoflive.backend.ris.model.TrainEvent
+import de.deutschebahn.bahnhoflive.backend.ris.model.TrainInfo
+import de.deutschebahn.bahnhoflive.repository.DbTimetableResource
+import de.deutschebahn.bahnhoflive.repository.LoadingStatus
+import de.deutschebahn.bahnhoflive.repository.MergedStation
+import de.deutschebahn.bahnhoflive.stream.rx.Optional
+import de.deutschebahn.bahnhoflive.ui.map.Content
+import de.deutschebahn.bahnhoflive.ui.map.InitialPoiManager.Companion.putInitialPoi
+import de.deutschebahn.bahnhoflive.ui.map.MapPresetProvider
+import de.deutschebahn.bahnhoflive.ui.station.HistoryFragment
+import de.deutschebahn.bahnhoflive.ui.station.StationViewModel
+import de.deutschebahn.bahnhoflive.ui.timetable.journey.JourneyFragment
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.functions.Consumer
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ViewAnimator;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
-import com.android.volley.VolleyError;
-
-import de.deutschebahn.bahnhoflive.R;
-import de.deutschebahn.bahnhoflive.analytics.TrackingManager;
-import de.deutschebahn.bahnhoflive.backend.ris.model.TrainEvent;
-import de.deutschebahn.bahnhoflive.backend.ris.model.TrainInfo;
-import de.deutschebahn.bahnhoflive.repository.DbTimetableResource;
-import de.deutschebahn.bahnhoflive.repository.LoadingStatus;
-import de.deutschebahn.bahnhoflive.repository.MergedStation;
-import de.deutschebahn.bahnhoflive.repository.timetable.Timetable;
-import de.deutschebahn.bahnhoflive.stream.rx.Optional;
-import de.deutschebahn.bahnhoflive.ui.map.Content;
-import de.deutschebahn.bahnhoflive.ui.map.InitialPoiManager;
-import de.deutschebahn.bahnhoflive.ui.map.MapPresetProvider;
-import de.deutschebahn.bahnhoflive.ui.map.content.rimap.Track;
-import de.deutschebahn.bahnhoflive.ui.station.HistoryFragment;
-import de.deutschebahn.bahnhoflive.ui.station.StationViewModel;
-import de.deutschebahn.bahnhoflive.ui.timetable.journey.JourneyFragment;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Consumer;
-import kotlin.Unit;
-
-public class DbTimetableFragment extends Fragment
-        implements FilterDialogFragment.Consumer, MapPresetProvider {
-
-    public static final String TAG = DbTimetableFragment.class.getSimpleName();
-
-    private DbTimetableAdapter adapter;
-    private ViewAnimator viewSwitcher;
-    private SwipeRefreshLayout swipeRefreshLayout;
-    private DbTimetableResource dbTimetableResource;
-    private CompositeDisposable disposable = new CompositeDisposable();
-    private MutableLiveData<TrainInfo> selectedTrainInfo;
-
-    private TrainInfo trainInfoFromMap = null;
-    private Boolean trainInfoFromMapSimulateClick = false;
-
-    public DbTimetableFragment() {
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        final StationViewModel stationViewModel = new ViewModelProvider(getActivity()).get(StationViewModel.class);
-        LiveData<MergedStation> stationLiveData = stationViewModel.getStationResource().getData();
-
-        dbTimetableResource = stationViewModel.getDbTimetableResource();
-
-        selectedTrainInfo = stationViewModel.getSelectedTrainInfo();
-
-        adapter = new DbTimetableAdapter(stationLiveData.getValue(), (trainCategories, trainCategory, tracks, track) -> {
-            final LiveData<Timetable> dbTimetableResourceData = dbTimetableResource.getData();
-            final Timetable timetable = dbTimetableResourceData.getValue();
-            final long endTime = timetable != null ? timetable.getEndTime() : 0;
-            final FilterDialogFragment filterDialogFragment = FilterDialogFragment.create(trainCategories, trainCategory, tracks, track, endTime);
-            filterDialogFragment.show(getChildFragmentManager(), "filterDialog");
-        }, getTrackingManager(), view -> {
-            dbTimetableResource.loadMore();
-        }, (trainInfo, trainEvent, integer) -> { // click on station in list
-            final HistoryFragment historyFragment = HistoryFragment.parentOf(this);
-            historyFragment.push(new JourneyFragment(trainInfo, trainEvent, false));
-            return Unit.INSTANCE;
-        });
-
-        stationLiveData.observe(this, station -> {
-            adapter.setStation(station);
-        });
-
-        dbTimetableResource.getData().observe(this, new Observer<Timetable>() {
-            @Override
-            public void onChanged(@Nullable Timetable timetable) {
-                if (timetable == null) {
-                    return;
-                }
-                adapter.setTimetable(timetable);
-                viewSwitcher.setDisplayedChild(0);
-                if (trainInfoFromMap != null) {
-                    trainInfoFromMapSimulateClick = true;
-                    selectedTrainInfo.setValue(trainInfoFromMap);
-                    trainInfoFromMap = null;
-                }
+class DbTimetableFragment : Fragment(), FilterDialogFragment.Consumer, MapPresetProvider {
+    private var adapter: DbTimetableAdapter? = null
+    private var viewSwitcher: ViewAnimator? = null
+    private var swipeRefreshLayout: SwipeRefreshLayout? = null
+    private var dbTimetableResource: DbTimetableResource? = null
+    private var disposable: CompositeDisposable? = CompositeDisposable()
+    private var selectedTrainInfo: MutableLiveData<TrainInfo>? = null
+    private var trainInfoFromMap: TrainInfo? = null
+    private var trainInfoFromMapSimulateClick = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val stationViewModel = ViewModelProvider(requireActivity()).get(
+            StationViewModel::class.java
+        )
+        val stationLiveData: LiveData<MergedStation> = stationViewModel.stationResource.data
+        dbTimetableResource = stationViewModel.dbTimetableResource
+        selectedTrainInfo = stationViewModel.selectedTrainInfo
+        adapter = DbTimetableAdapter(
+            stationLiveData.value,
+            { trainCategories: Array<String?>?, trainCategory: String?, tracks: Array<String?>?, track: String? ->
+                val dbTimetableResourceData = dbTimetableResource!!.data
+                val timetable = dbTimetableResourceData.value
+                val endTime = timetable?.endTime ?: 0
+                val filterDialogFragment = FilterDialogFragment.create(
+                    trainCategories,
+                    trainCategory,
+                    tracks,
+                    track,
+                    endTime
+                )
+                filterDialogFragment.show(childFragmentManager, "filterDialog")
+            },
+            trackingManager,
+            { view: View? -> dbTimetableResource!!.loadMore() }) { trainInfo: TrainInfo?, trainEvent: TrainEvent?, integer: Int? ->  // click on station in list
+            val historyFragment = HistoryFragment.parentOf(this)
+            historyFragment.push(JourneyFragment(trainInfo!!, trainEvent!!, false))
+            Unit
+        }
+        stationLiveData.observe(this) { station: MergedStation? -> adapter!!.setStation(station) }
+        dbTimetableResource!!.data.observe(this, Observer { timetable ->
+            if (timetable == null) {
+                return@Observer
             }
-        });
-
-        dbTimetableResource.getError().observe(this, new Observer<VolleyError>() {
-            @Override
-            public void onChanged(@Nullable VolleyError volleyError) {
-                if (volleyError != null) {
-                    viewSwitcher.setDisplayedChild(2);
+            adapter!!.setTimetable(timetable)
+            viewSwitcher!!.displayedChild = 0
+            if (trainInfoFromMap != null) {
+                trainInfoFromMapSimulateClick = true
+                trainInfoFromMap?.let {
+                    selectedTrainInfo?.setValue(it)
                 }
+                trainInfoFromMap = null
             }
-        });
-        dbTimetableResource.getLoadingStatus().observe(this, new Observer<LoadingStatus>() {
-            @Override
-            public void onChanged(@Nullable LoadingStatus loadingStatus) {
-                swipeRefreshLayout.setRefreshing(loadingStatus == LoadingStatus.BUSY);
-                if (loadingStatus == LoadingStatus.BUSY) {
-                    viewSwitcher.setDisplayedChild(1);
-                }
+        })
+        dbTimetableResource!!.error.observe(this) { volleyError ->
+            if (volleyError != null) {
+                viewSwitcher!!.displayedChild = 2
             }
-        });
-
-        disposable.add(stationViewModel.getTrackFilterObservable().subscribe(new Consumer<Optional<String>>() {
-            @Override
-            public void accept(Optional<String> trackFilter) {
-                setFilter(trackFilter.getValue());
+        }
+        dbTimetableResource!!.loadingStatus.observe(this) { loadingStatus ->
+            swipeRefreshLayout!!.isRefreshing = loadingStatus == LoadingStatus.BUSY
+            if (loadingStatus == LoadingStatus.BUSY) {
+                viewSwitcher!!.displayedChild = 1
             }
-        }));
+        }
+        disposable!!.add(stationViewModel.trackFilterObservable.subscribe(Consumer<Optional<String>> { trackFilter ->
+            setFilter(
+                trackFilter.value
+            )
+        }))
 
         // Observer fuer Wagenreihungs-Anzeige (kommt leider BEVOR die traininfos geladen sind...)
         // kommt letztendlich aus der map
-        disposable.add(stationViewModel.getWaggonOrderObservable().subscribe(new Consumer<TrainInfo>() {
-            @Override
-            public void accept(TrainInfo trainInfo) {
-                trainInfoFromMap = trainInfo;
-            }
-        }));
+        disposable!!.add(stationViewModel.waggonOrderObservable.subscribe { trainInfo ->
+            trainInfoFromMap = trainInfo
+        })
     }
 
+    val trackingManager: TrackingManager
+        get() = fromActivity(activity)
 
-    @NonNull
-    public TrackingManager getTrackingManager() {
-        final TrackingManager trackingManager = TrackingManager.fromActivity(getActivity());
-        return trackingManager;
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.fragment_timetable_db, container, false);
-
-        swipeRefreshLayout = view.findViewById(R.id.refresher);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                dbTimetableResource.refresh();
-            }
-        });
-
-        final RecyclerView recyclerView = view.findViewById(R.id.recycler);
-        recyclerView.setAdapter(adapter);
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_timetable_db, container, false)
+        swipeRefreshLayout = view.findViewById(R.id.refresher)
+        swipeRefreshLayout?.setOnRefreshListener(SwipeRefreshLayout.OnRefreshListener { dbTimetableResource!!.refresh() })
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler)
+        recyclerView.adapter = adapter
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    getTrackingManager().track(TrackingManager.TYPE_ACTION, TrackingManager.Screen.H2, TrackingManager.UiElement.LIST, TrackingManager.UiElement.DEPARTURE, TrackingManager.Action.SCROLL);
+                    trackingManager.track(
+                        TrackingManager.TYPE_ACTION,
+                        TrackingManager.Screen.H2,
+                        TrackingManager.UiElement.LIST,
+                        TrackingManager.UiElement.DEPARTURE,
+                        TrackingManager.Action.SCROLL
+                    )
                 }
             }
-        });
-
-        viewSwitcher = view.findViewById(R.id.switcher);
-
-        selectedTrainInfo.observe(getViewLifecycleOwner(), trainInfo -> {
+        })
+        viewSwitcher = view.findViewById(R.id.switcher)
+        selectedTrainInfo!!.observe(viewLifecycleOwner) { trainInfo: TrainInfo? ->
             if (trainInfo != null) {
-                final int itemIndex = adapter.setSelectedItem(trainInfo);
+                val itemIndex = adapter!!.setSelectedItem(trainInfo)
                 if (itemIndex >= 0) {
-                    recyclerView.scrollToPosition(itemIndex);
-                    if(trainInfoFromMapSimulateClick) {
-                        trainInfoFromMapSimulateClick = false;
-                        TrainEvent trainEvent = TrainEvent.DEPARTURE;
-                        final HistoryFragment historyFragment = HistoryFragment.parentOf(this);
-                        historyFragment.push(new JourneyFragment(trainInfo, trainEvent, true));
+                    recyclerView.scrollToPosition(itemIndex)
+                    if (trainInfoFromMapSimulateClick) {
+                        trainInfoFromMapSimulateClick = false
+                        val trainEvent = TrainEvent.DEPARTURE
+                        val historyFragment = HistoryFragment.parentOf(this)
+                        historyFragment.push(JourneyFragment(trainInfo, trainEvent, true))
                     }
-
                 }
-                selectedTrainInfo.setValue(null);
-                trainInfoFromMapSimulateClick = false;
+                selectedTrainInfo!!.setValue(null)
+                trainInfoFromMapSimulateClick = false
             }
-        });
-
-        return view;
+        }
+        return view
     }
 
-    @Override
-    public void onDestroyView() {
-        viewSwitcher = null;
-        swipeRefreshLayout = null;
-
-        super.onDestroyView();
+    override fun onDestroyView() {
+        viewSwitcher = null
+        swipeRefreshLayout = null
+        super.onDestroyView()
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
+    override fun onDestroy() {
+        super.onDestroy()
         if (disposable != null) {
-            disposable.dispose();
-            disposable = null;
+            disposable!!.dispose()
+            disposable = null
         }
     }
 
-    @Override
-    public void setFilter(String trainCategory, String track) {
-        adapter.setFilter(trainCategory, track);
+    override fun setFilter(trainCategory: String, track: String) {
+        adapter!!.setFilter(trainCategory, track)
     }
 
-    public void setFilter(String track) {
-        adapter.setFilter(track);
+    fun setFilter(track: String?) {
+        adapter!!.setFilter(track)
     }
 
-    @Override
-    public boolean prepareMapIntent(@NonNull Intent intent) {
-        final Track track = adapter.getCurrentTrack();
-
+    override fun prepareMapIntent(intent: Intent): Boolean {
+        val track = adapter!!.currentTrack
         if (track != null) {
-            InitialPoiManager.putInitialPoi(intent, Content.Source.RIMAP, track);
-            return true;
+            putInitialPoi(intent, Content.Source.RIMAP, track)
+            return true
         }
-
-        return false;
+        return false
     }
 
-    public void setModeAndFilter(boolean arrivals, String trackFilter) {
-        setFilter(trackFilter);
-        adapter.setArrivals(arrivals);
+    fun setModeAndFilter(arrivals: Boolean, trackFilter: String?) {
+        setFilter(trackFilter)
+        adapter!!.setArrivals(arrivals)
+    }
+
+    companion object {
+        @JvmField
+        val TAG = DbTimetableFragment::class.java.simpleName
     }
 }
