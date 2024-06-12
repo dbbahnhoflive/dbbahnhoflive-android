dd2ae04a64d1a41f49749a8d66773f6f665b93c0
Codeelements_Ruhe <c.ruhe@codeelements.de>
2024-06-12 15:26:30 +0200

    2518 - change legal_notice for app

--- modules/core/src/main/java/de/deutschebahn/bahnhoflive/repository/AssetDocumentBroker.kt
+++ modules/core/src/main/java/de/deutschebahn/bahnhoflive/repository/AssetDocumentBroker.kt
@@ -20,7 +20,7 @@ class AssetDocumentBroker(
     companion object {
         const val FILE_NAME_LEGAL_NOTICE_EXPIRING = "impressum.html"
         const val FILE_NAME_LEGAL_NOTICE_2024_01_01 = "impressum_2024-01-01.html"
-        const val FILE_NAME_PRIVACY_POLICY_2023_08_15 = "datenschutz_2023-08-15.html"
+        const val FILE_NAME_PRIVACY_POLICY_2024_07_01 = "datenschutz_2024-07-01.html"
         const val FILE_NAME_PRIVACY_POLICY_2024_01_01 = "datenschutz_2024-01-01.html"
     }
 
@@ -44,10 +44,10 @@ class AssetDocumentBroker(
             FILE_NAME_LEGAL_NOTICE_2024_01_01, TrackingManager.Entity.IMPRESSUM, "Impressum"
         ),
         PRIVACY_POLICY_EXPIRING(
-            FILE_NAME_PRIVACY_POLICY_2023_08_15, TrackingManager.Entity.DATENSCHUTZ, "Datenschutz"
+            FILE_NAME_PRIVACY_POLICY_2024_01_01, TrackingManager.Entity.DATENSCHUTZ, "Datenschutz"
         ),
         PRIVACY_POLICY_UPCOMING(
-            FILE_NAME_PRIVACY_POLICY_2024_01_01, TrackingManager.Entity.DATENSCHUTZ, "Datenschutz"
+            FILE_NAME_PRIVACY_POLICY_2024_07_01, TrackingManager.Entity.DATENSCHUTZ, "Datenschutz"
         )
     }
 
@@ -63,8 +63,12 @@ class AssetDocumentBroker(
     }
 
     fun getCurrentPrivacyPolicy() =
-        Calendar.getInstance(Locale.GERMANY).get(Calendar.YEAR).let { currentYear ->
-            if (currentYear < 2024) Document.PRIVACY_POLICY_EXPIRING else Document.PRIVACY_POLICY_UPCOMING
+        Calendar.getInstance(Locale.GERMANY).let { calendar ->
+            if (calendar.get(Calendar.YEAR) <= 2024 &&
+                calendar.get(Calendar.MONTH) < Calendar.JULY
+            )
+                Document.PRIVACY_POLICY_EXPIRING
+            else Document.PRIVACY_POLICY_UPCOMING
         }
 
     fun getCurrentLegalNotice() =
